b17f60a95bd93b4b61244deeb3dd1bd50197fa6e
davdelpal1 <delgado.pallares.david@gmail.com>
2025-04-11 03:51:30 +0200

    Añadiendo app web y archivos de configuración de Firebase

---
+++ mobile/src/config/firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBa48T8X9LC95zI5lKZpOAD5DalmGuCytI",
+  authDomain: "hermandapp-bed16.firebaseapp.com",
+  projectId: "hermandapp-bed16",
+  storageBucket: "hermandapp-bed16.firebasestorage.app",
+  messagingSenderId: "326574651601",
+  appId: "1:326574651601:web:fc34c05902155f2b322df0"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
